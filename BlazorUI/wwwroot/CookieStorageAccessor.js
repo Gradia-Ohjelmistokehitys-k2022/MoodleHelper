b90a3b662bc2bc05fe34e609252439c4ae01249f
osaama05 <oskari.makela@hotmail.com>
2023-11-08 13:02:51 +0200

    Made ui (add copying to clipboard)

---
+++ BlazorUI/wwwroot/CookieStorageAccessor.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CookieStorageAccessor', CookieStorageAccessor);
+
+    CookieStorageAccessor.$inject = ['$location'];
+
+    function CookieStorageAccessor($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'CookieStorageAccessor';
+
+        activate();
+
+        function activate() { }
+
+        export function get() {
+            return document.cookie;
+        }
+
+        export function set(key, value) {
+            document.cookie = `${key}=${value}`;
+        }
+    }
+})();
