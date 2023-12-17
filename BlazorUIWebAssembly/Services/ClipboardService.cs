343dead61db0fa6971ebefc5d1ca56797762fdb0
osaama05 <oskari.makela@hotmail.com>
2023-12-18 01:45:47 +0200

    Fixed temporarily hardcoded navmenu

---
+++ BlazorUIWebAssembly/Services/ClipboardService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorUIWebAssembly.Services
+{
+    public class ClipboardService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ClipboardService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
