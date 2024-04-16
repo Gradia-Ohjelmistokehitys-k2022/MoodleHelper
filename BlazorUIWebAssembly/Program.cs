10e9aeec6a22d1b10f37d90c39dd65e8d5dcc4f5
Roope Lehtomäki <ac2141@student.jamk.fi>
2024-04-16 13:12:58 +0300

    Work of few months, most in ShortAnswer, which will be later copied to the others. "Minor" issue in shortanswer's first foreach loop.

--- BlazorUIWebAssembly/Program.cs
+++ BlazorUIWebAssembly/Program.cs
@@ -1,5 +1,6 @@
 using BlazorBootstrap;
 using BlazorUIWebAssembly;
+using BlazorUIWebAssembly.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
--- BlazorUIWebAssembly/Services/ClipboardService.cs
+++ BlazorUIWebAssembly/Services/ClipboardService.cs
@@ -4,30 +4,21 @@ using BlazorUIWebAssembly.Shared;
 
 namespace BlazorUIWebAssembly.Services
 {
-    public class ClipboardService
+    public interface IClipboardService
     {
-        private readonly IJSRuntime _jsRuntime;
-        private readonly Popup popup = new();
-
-        public ClipboardService(IJSRuntime jsRuntime)
+        Task CopyToClipboard(string text);
+    }
+    public class ClipboardService : IClipboardService
+    {
+        private readonly IJSRuntime _jsInterop;
+        
+        public ClipboardService(IJSRuntime jsInterop)
         {
-            _jsRuntime = jsRuntime;
-        }
-
-        public ValueTask WriteTextAsync(string text)
-        {
-            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+            _jsInterop = jsInterop;
         }
         public async Task CopyToClipboard(string text)
         {
-            try
-            {
-                await WriteTextAsync(text);
-            }
-            catch
-            {
-                popup.Show("Failed to copy to clipboard", "Error");
-            }
+            await _jsInterop.InvokeVoidAsync("navigator.clipboard.writeText");
         }
     }
 }
