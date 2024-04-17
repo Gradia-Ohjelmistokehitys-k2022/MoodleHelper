f6b92107665c55854fba7a2ef05f72e741e99e4d
osaama05 <oskari.makela@hotmail.com>
2024-04-17 10:40:38 +0300

    Added option to backend for defining the percentage of points the answer gives

--- SyntaxGenerator/Models/AnswerOption.cs
+++ SyntaxGenerator/Models/AnswerOption.cs
@@ -2,14 +2,10 @@
 {
     public class AnswerOption
     {
-        public string? Text { get; set; }
+        public string Text { get; set; }
         public bool IsCorrect { get; set; }
         public string? Feedback { get; set; }
-
-        public AnswerOption()
-        {
-            
-        }
+        public int Points { get; set; }
 
         public AnswerOption(string text, bool isCorrect)
         {
@@ -17,11 +13,26 @@
             IsCorrect = isCorrect;
         }
 
+        public AnswerOption(string text, bool isCorrect, int points)
+        {
+            Text = text;
+            IsCorrect = isCorrect;
+            Points = points;
+        }
+
         public AnswerOption(string text, bool isCorrect, string feedback)
         {
             Text = text;
             IsCorrect = isCorrect;
             Feedback = feedback;
+        }
+
+        public AnswerOption(string text, bool isCorrect, string feedback, int points)
+        {
+            Text = text;
+            IsCorrect = isCorrect;
+            Feedback = feedback;
+            Points = points;
         }
     }
 }
